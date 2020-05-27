b7bb83caf2773f581c553f943373e178f65f6e50
Monark <monark@factly.in>
2020-05-27 12:07:00 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import "antd/dist/antd.css";
+import 'antd/dist/antd.css';
 
-import BasicLayout from "./layout/basic";
-import Tags from "./pages/tags/index";
-import Memberships from "./pages/memberships/index";
-import Payments from "./pages/payments/index";
-import TagCreate from "./pages/tags/create";
-import Plans from "./pages/plans/index";
-import PlanCreate from "./pages/plans/create";
+import BasicLayout from './layout/basic';
+import Tags from './pages/tags/index';
+import Memberships from './pages/memberships/index';
+import Payments from './pages/payments/index';
+import TagCreate from './pages/tags/create';
+import Plans from './pages/plans/index';
+import PlanCreate from './pages/plans/create';
+import Login from './pages/login';
+import Registration from './pages/registration';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <BasicLayout>
-          <Switch>
-            <Route exact path={process.env.PUBLIC_URL + "/"} />
-            <Route exact path={process.env.PUBLIC_URL + "/tags/create"} component={TagCreate} />
-            <Route exact path={process.env.PUBLIC_URL + "/tags"} component={Tags} />
-            <Route exact path={process.env.PUBLIC_URL + "/plans/create"} component={PlanCreate} />
-            <Route exact path={process.env.PUBLIC_URL + "/plans"} component={Plans} />
-            <Route exact path={process.env.PUBLIC_URL + "/memberships"} component={Memberships} />
-            <Route exact path={process.env.PUBLIC_URL + "/payments"} component={Payments} />
-          </Switch>
-        </BasicLayout>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/auth/login" component={Login} />
+          <Route path="/auth/registration" component={Registration} />
+          <BasicLayout>
+            <Route exact path={'/'} />
+            <Route exact path={'/tags/create'} component={TagCreate} />
+            <Route exact path={'/tags'} component={Tags} />
+            <Route exact path={'/plans/create'} component={PlanCreate} />
+            <Route exact path={'/plans'} component={Plans} />
+            <Route exact path={'/memberships'} component={Memberships} />
+            <Route exact path={'/payments'} component={Payments} />
+          </BasicLayout>
+        </Switch>
       </Router>
     </div>
   );
