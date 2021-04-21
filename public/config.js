f097292acd613b0a62e151fec5d6330fd4bec270
shreeharsha-factly <harsha@factly.in>
2021-04-22 01:26:57 +0530

    change data-portal to mande

--- public/config.js
+++ public/config.js
@@ -1,2 +1,3 @@
 window.REACT_APP_KRATOS_PUBLIC_URL = 'http://127.0.0.1:4455/.ory/kratos/public';
-window.REACT_APP_API_URL = 'http://127.0.0.1:4455/.factly/data-portal/server/admin';
+window.REACT_APP_API_URL = 'http://127.0.0.1:4455/.factly/mande/server/admin';
+window.PUBLIC_URL=  'http://127.0.0.1:4455/.factly/mande/studio';
