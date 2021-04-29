5665aa6530420da7eab5c3de931acd5705770732
shreeharsha-factly <harsha@factly.in>
2021-04-29 21:33:50 +0530

    add axios middleware

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-export default axios.create({
-  baseURL: window.REACT_APP_API_URL,
-  headers: {
-    common: {
-      'X-Organisation': '1',
-    },
-  },
-});
+function createAxiosAuthMiddleware() {
+  return ({ getState }) => (next) => (action) => {
+
+    console.log("selected",getState().organisations.selected)
+    axios.defaults.headers.common['X-Organisation'] = getState().organisations.selected;
+    axios.defaults.baseURL = window.REACT_APP_API_URL;
+    axios.defaults.withCredentials = true;
+    return next(action);
+  };
+}
+
+const axiosAuth = createAxiosAuthMiddleware();
+
+export default axiosAuth;
