ca853e39dfbd02a0a956c4409f68dd7dcdfb9ef4
shreeharsha-factly <harsha@factly.in>
2021-05-18 12:40:17 +0530

    feat: add updated fields in catalog, product and dataset

--- src/pages/datasets/components/DatasetDetails.js
+++ src/pages/datasets/components/DatasetDetails.js
@@ -18,22 +18,40 @@ const DatasetDetails = ({ dataset }) => {
             onClick={() => history.push(`/datasets/${dataset.id}/edit`)}
           />
         }
+        bordered
       >
+        <Descriptions.Item label="Organisation">{dataset.organisation}</Descriptions.Item>
+        <Descriptions.Item label="Sectors">{dataset.sectors}</Descriptions.Item>
         <Descriptions.Item label="Contact email">{dataset.contact_email}</Descriptions.Item>
         <Descriptions.Item label="Contact name">{dataset.contact_name}</Descriptions.Item>
         <Descriptions.Item label="Data standard">{dataset.data_standard}</Descriptions.Item>
-        <Descriptions.Item label="Description">{dataset.description}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          <div
+            style={{ whiteSpace: 'pre-wrap' }}
+            dangerouslySetInnerHTML={{ __html: dataset.description }}
+          />
+        </Descriptions.Item>
         <Descriptions.Item label="Frequency">{dataset.frequency}</Descriptions.Item>
         <Descriptions.Item label="Granularity">{dataset.granularity}</Descriptions.Item>
         <Descriptions.Item label="License">{dataset.license}</Descriptions.Item>
         <Descriptions.Item label="Related articles">{dataset.related_articles}</Descriptions.Item>
         <Descriptions.Item label="Source">{dataset.source}</Descriptions.Item>
+        <Descriptions.Item label="Source Link">{dataset.source_link}</Descriptions.Item>
+        <Descriptions.Item label="Archive Link">{dataset.archive_link}</Descriptions.Item>
         <Descriptions.Item label="Temporal coverage">{dataset.temporal_coverage}</Descriptions.Item>
         <Descriptions.Item label="Time saved">{dataset.time_saved}</Descriptions.Item>
-        <Descriptions.Item label="Profiling URL">{dataset.profiling_url}</Descriptions.Item>
+        <Descriptions.Item label="Profiling URL">
+          <a>{dataset.profiling_url}</a>
+        </Descriptions.Item>
         <Descriptions.Item label="Is Public">{dataset.is_public ? 'yes' : 'no'}</Descriptions.Item>
         <Descriptions.Item label="Created At">
-          {moment(dataset.created_at).fromNow()}
+          {moment(dataset.created_at).format('Do MMMM YYYY')}
+        </Descriptions.Item>
+        <Descriptions.Item label="Last Updated At">
+          {moment(dataset.updated_at).format('Do MMMM YYYY')}
+        </Descriptions.Item>
+        <Descriptions.Item label="Next Update">
+          {dataset.next_update ? moment(dataset.next_update).format('Do MMMM YYYY') : null}
         </Descriptions.Item>
       </Descriptions>
     </Card>
